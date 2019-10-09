cc6275b26a6293502014eec45ec3bbd385a4ab65
M . Asaad <47598955+ooatamelbug@users.noreply.github.com>
2019-10-09 11:22:09 +0200

    Add files via upload
    add

---
+++ includes/functions/nav.php
@@ -0,0 +1,43 @@
+<div class="span3" id="sidebar">
+                                        <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+                                            <li class="active">
+                                                <a href="index.php?page=histoory&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> histoory</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=examination&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> examination</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=reimplant&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> reimplant</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=3_months&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 3_months</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=6_months&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 6_months</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=9_months&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 9_months</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=yrs_5_2&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> yrs_5_2</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=yrs_5_3&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> yrs_5_3</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=yr_5_1&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> yr_5_1</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=2yrs&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 2yrs</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=3yrs&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 3yrs</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=4yrs&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> 4yrs</a>
+                                            </li>
+                                            <li>
+                                            <a href="index.php?page=yr_1&id=<?=$_GET['id']?>"><i class="icon-chevron-right"></i> yr_1</a>
+                                            </li>
+                                        </ul>
+                                    </div>
---
+++ includes/functions/side.php
@@ -0,0 +1,20 @@
+<div class="alert alert-success">
+                                                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                                <h4>Success</h4>
+                                                The operation completed successfully</div>
+                                                <div class="navbar">
+                                                    <div class="navbar-inner">
+                                                        <ul class="breadcrumb">
+                                                            <i class="icon-chevron-left hide-sidebar"><a href='#' title="Hide Sidebar" rel='tooltip'>&nbsp;</a></i>
+                                                            <i class="icon-chevron-right show-sidebar" style="display:none;"><a href='#' title="Show Sidebar" rel='tooltip'>&nbsp;</a></i>
+                                                            <li>
+                                                                <a href="search.php">Dashboard</a> <span class="divider">/</span>	
+                                                            </li>
+                                                            <li>
+                                                                <a href="index.php?page=<?=$_GET['page']?>&id=<?=$_GET['id']?>"><?=$_GET['page']?></a> <span class="divider">/</span>	
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        
